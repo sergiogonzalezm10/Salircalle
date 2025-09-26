ec643db4db7e48cf5814e2448d637497e5bf7740
sergio.gonzalezm <sergio.gonzalezm@106-09.info2>
2025-09-26 12:22:17 +0200

    Salircalle

--- src/salircalle/Salircalle.java
+++ src/salircalle/Salircalle.java
@@ -6,14 +6,14 @@ public class Salircalle {
 
 	public static void main(String[] args) {
 		
-		boolean llueve;
-		boolean tareasFinalizadas;
-		boolean irBiblioteca;
-		boolean puedeSalir;
+		boolean llueve;					//Variable boolean para devolver true o false para si llueve o no
+		boolean tareasFinalizadas;		//Variable boolean para devolver true o false para si tareas finalizadas o no
+		boolean irBiblioteca;			//Variable boolean para devolver true o false para si hay que ir a la biblioteca o no
+		boolean puedeSalir;				//Variable boolean para devolver true o false para si se puede salir o no
 		
-		  Scanner sc = new Scanner(System.in);
+		  Scanner sc = new Scanner(System.in);		
 
-	        System.out.print("¿Está lloviendo?: ");
+	        System.out.print("¿Está lloviendo?: ");			
 	        llueve = sc.nextBoolean();  
 
 	        System.out.print("¿Has terminado tus tareas?: ");
@@ -22,9 +22,9 @@ public class Salircalle {
 	        System.out.print("¿Necesitas ir a la biblioteca?): ");
 	        irBiblioteca = sc.nextBoolean();
 
-	         puedeSalir = (!llueve && tareasFinalizadas) || irBiblioteca;
+	         puedeSalir = (!llueve && tareasFinalizadas) || irBiblioteca;		//Utilizamos y jugamos con NOT , AND y OR para solucionar el problema.
 
-	        System.out.println("¿Puedes salir a la calle? " + puedeSalir);
+	        System.out.println("¿Puedes salir a la calle? " + (puedeSalir? "Si" : "No"));	
 
 	        sc.close();
 	    }
